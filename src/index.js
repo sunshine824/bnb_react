a9fa4732c53c0a0cf8100b9fce7ae6884397fec3
Administrator <790168710@qq.com>
2017-12-05 18:33:31 +0800

    first commit

--- src/index.js
+++ src/index.js
@@ -3,5 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './static/css/base.css'
+
+ReactDOM.render(<App/>, document.getElementById('root'));
 registerServiceWorker();
