781c098573be3ea01e332484b433aba5c63d211a
Administrator <790168710@qq.com>
2017-12-08 17:31:58 +0800

    fisrt commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux'
-import store from './redux/store'
+//import {Provider} from 'react-redux'
+//import store from './redux/store'
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 import './static/css/base.css'
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App/>
-    </Provider>, document.getElementById('root'));
+    <App/>, document.getElementById('root'));
 registerServiceWorker();
