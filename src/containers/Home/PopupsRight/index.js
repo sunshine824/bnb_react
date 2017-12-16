84b5142bb48b4333416a21e1aece0ce41a9e0bf0
sunshine824 <790168710@qq.com>
2017-12-16 17:00:53 +0800

    右侧slide

--- src/containers/Home/PopupsRight/index.js
+++ src/containers/Home/PopupsRight/index.js
@@ -1,8 +1,9 @@
 import React, {Component} from 'react'
-import {DatePicker, Radio, Select, Input, Button} from 'antd';
+import {DatePicker, Form, Radio, Select, Input, Button} from 'antd';
 
 import './style.less'
 
+const FormItem = Form.Item;
 const {RangePicker} = DatePicker;
 const RadioGroup = Radio.Group;
 const Option = Select.Option;
@@ -49,134 +50,162 @@ class PopupsRight extends Component {
 
     slideOpen() {
         this.setState({
-            slide_open:!this.state.slide_open
+            slide_open: !this.state.slide_open
         })
     }
 
-    onSubmit(){
+    onSubmit() {
         console.log(1)
     }
 
+    handleSubmit(e) {
+        e.preventDefault();
+        this.props.form.validateFields((err, values) => {
+            if (!err) {
+                console.log('Received values of form: ', values);
+            }
+        });
+    }
+
     render() {
-        const className= this.state.slide_open ? 'active' : ''
+        const className = this.state.slide_open ? 'active' : ''
+        const {getFieldDecorator} = this.props.form;
         return (
-            <div className={"content-slide " +  className}>
-                <p className="title">编辑订单</p>
-                <div className="slide-body">
+            <Form onSubmit={this.handleSubmit.bind(this)}>
+                <div className={"content-slide " + className}>
+                    <p className="title">编辑订单</p>
+                    <div className="slide-body">
+
+                        <p className="group-title">订单信息</p>
+                        <div className="item">
+                            <p className="item-title">
+                                入住日期
+                                <span>共2晚</span>
+                            </p>
+                            <div className="check_input">
+                                <FormItem>
+                                    {getFieldDecorator('checkIn', {
+                                        rules: [{required: true, message: '请选择入住日期'}]
+                                    })(
+                                        <RangePicker
+                                            format="YYYY-MM-DD"
+                                            placeholder={['入住日期', '退房日期']}
+                                            onChange={this.onChangeDate.bind(this)}
+                                        />
+                                    )}
+                                </FormItem>
+                            </div>
+                        </div>
+                        <div className="item">
+                            <p className="item-title">
+                                订单状态
+                            </p>
+                            <div className="check_input">
+                                <FormItem>
+                                    {getFieldDecorator('status',{
+                                        rules:[{required:true, message:'请选择状态'}]
+                                    })(
+                                        <RadioGroup onChange={this.onChangeRadio.bind(this, 'order')}
+                                                    value={this.state.order_value}>
+                                            <Radio value={1}>预订</Radio>
+                                            <Radio value={2}>屏蔽</Radio>
+                                        </RadioGroup>
+                                    )}
+                                </FormItem>
+                            </div>
+                        </div>
+                        <div className="item">
+                            <p className="item-title">
+                                渠道来源
+                            </p>
+                            <div className="check_input">
+                                <Select
+                                    showSearch
+                                    style={{width: '100%'}}
+                                    placeholder="选择渠道来源"
+                                    optionFilterProp="children"
+                                    onChange={this.handleChange.bind(this)}
+                                    filterOption={(input, option) => option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
+                                >
+                                    <Option value="jack">58同城</Option>
+                                    <Option value="lucy">优客逸家</Option>
+                                    <Option value="tom">居房源</Option>
+                                </Select>
+                            </div>
+                        </div>
+                        <div className="item">
+                            <p className="item-title">
+                                订单收入
+                            </p>
+                            <div className="check_input">
+                                <Input addonBefore="￥" placeholder="0" onBlur={this.onBlurInput.bind(this, 'mold')}/>
+                            </div>
+                        </div>
 
 
-                    <p className="group-title">订单信息</p>
-                    <div className="item">
-                        <p className="item-title">
-                            入住日期
-                            <span>共2晚</span>
-                        </p>
-                        <div className="check_input">
-                            <RangePicker
-                                format="YYYY-MM-DD"
-                                placeholder={['入住日期', '退房日期']}
-                                onChange={this.onChangeDate.bind(this)}
-                            />
+                        <p className="group-title">入住人信息</p>
+                        <div className="item">
+                            <p className="item-title">
+                                姓名
+                            </p>
+                            <div className="check_input">
+                                <Input placeholder="入住人姓名" onBlur={this.onBlurInput.bind(this, 'name')}/>
+                            </div>
                         </div>
-                    </div>
-                    <div className="item">
-                        <p className="item-title">
-                            订单状态
-                        </p>
-                        <div className="check_input">
-                            <RadioGroup onChange={this.onChangeRadio.bind(this, 'order')}
-                                        value={this.state.order_value}>
-                                <Radio value={1}>预订</Radio>
-                                <Radio value={2}>屏蔽</Radio>
-                            </RadioGroup>
+                        <div className="item">
+                            <p className="item-title">
+                                电话
+                            </p>
+                            <div className="check_input">
+                                <Input placeholder="入住人电话" onBlur={this.onBlurInput.bind(this, 'phone')}/>
+                            </div>
                         </div>
-                    </div>
-                    <div className="item">
-                        <p className="item-title">
-                            渠道来源
-                        </p>
-                        <div className="check_input">
-                            <Select
-                                showSearch
-                                style={{width: '100%'}}
-                                placeholder="选择渠道来源"
-                                optionFilterProp="children"
-                                onChange={this.handleChange.bind(this)}
-                                filterOption={(input, option) => option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
-                            >
-                                <Option value="jack">58同城</Option>
-                                <Option value="lucy">优客逸家</Option>
-                                <Option value="tom">居房源</Option>
-                            </Select>
+                        <div className="item">
+                            <p className="item-title">
+                                微信号
+                            </p>
+                            <div className="check_input">
+                                <Input placeholder="入住人微信号" onBlur={this.onBlurInput.bind(this, 'weChat')}/>
+                            </div>
                         </div>
-                    </div>
-                    <div className="item">
-                        <p className="item-title">
-                            订单收入
-                        </p>
-                        <div className="check_input">
-                            <Input addonBefore="￥" placeholder="0" onBlur={this.onBlurInput.bind(this, 'mold')}/>
-                        </div>
-                    </div>
 
 
-                    <p className="group-title">入住人信息</p>
-                    <div className="item">
-                        <p className="item-title">
-                            姓名
-                        </p>
-                        <div className="check_input">
-                            <Input placeholder="入住人姓名" onBlur={this.onBlurInput.bind(this, 'name')}/>
+                        <p className="group-title">订单备注</p>
+                        <div className="item">
+                            <div className="check_input">
+                                <TextArea placeholder="备注信息" rows={4} onBlur={this.onBlurInput.bind(this, 'remark')}/>
+                            </div>
                         </div>
-                    </div>
-                    <div className="item">
-                        <p className="item-title">
-                            电话
-                        </p>
-                        <div className="check_input">
-                            <Input placeholder="入住人电话" onBlur={this.onBlurInput.bind(this, 'phone')}/>
-                        </div>
-                    </div>
-                    <div className="item">
-                        <p className="item-title">
-                            微信号
-                        </p>
-                        <div className="check_input">
-                            <Input placeholder="入住人微信号" onBlur={this.onBlurInput.bind(this, 'weChat')}/>
-                        </div>
-                    </div>
 
-
-                    <p className="group-title">订单备注</p>
-                    <div className="item">
-                        <div className="check_input">
-                            <TextArea placeholder="备注信息" rows={4} onBlur={this.onBlurInput.bind(this, 'remark')}/>
+                        <p className="group-title">颜色选择</p>
+                        <div className="item">
+                            <div className="check_input">
+                                <RadioGroup onChange={this.onChangeRadio.bind(this, 'color')}
+                                            value={this.state.color_value}>
+                                    <Radio value={1}></Radio>
+                                    <Radio value={2}></Radio>
+                                    <Radio value={3}></Radio>
+                                    <Radio value={4}></Radio>
+                                </RadioGroup>
+                            </div>
                         </div>
+
                     </div>
+                    <div className="btn-footer">
+                        <Button type="primary" htmlType="submit"
+                                style={{float: 'left', width: '100px'}}>确定</Button>
+                        <Button onClick={this.slideOpen.bind(this)} style={{float: 'right', width: '100px'}}>取消</Button>
+                    </div>
+                    <div className="slide-btn" onClick={this.slideOpen.bind(this)}>
 
-                    <p className="group-title">颜色选择</p>
-                    <div className="item">
-                        <div className="check_input">
-                            <RadioGroup onChange={this.onChangeRadio.bind(this, 'color')}
-                                        value={this.state.color_value}>
-                                <Radio value={1}></Radio>
-                                <Radio value={2}></Radio>
-                                <Radio value={3}></Radio>
-                                <Radio value={4}></Radio>
-                            </RadioGroup>
-                        </div>
                     </div>
                 </div>
-                <div className="btn-footer">
-                    <Button type="primary" onClick={this.onSubmit.bind(this)} style={{float: 'left', width: '100px'}}>确定</Button>
-                    <Button onClick={this.slideOpen.bind(this)} style={{float: 'right', width: '100px'}}>取消</Button>
-                </div>
-                <div className="slide-btn" onClick={this.slideOpen.bind(this)}>
-
-                </div>
-            </div>
+            </Form>
         )
     }
 }
 
-export default PopupsRight
+
+const WrappedPopupsRight = Form.create()(PopupsRight);
+
+export default WrappedPopupsRight
